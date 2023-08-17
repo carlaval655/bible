c4a363718fbf28ccd2eb2c587860b52081e4c563
carlaval655 <carla.val655@gmail.com>
2023-08-16 20:35:45 -0400

    Creando proyecto

---
+++ src/main/kotlin/bo/edu/ucb/bible/BibleApplication.kt
@@ -0,0 +1,11 @@
+package bo.edu.ucb.bible
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BibleApplication
+
+fun main(args: Array<String>) {
+	runApplication<BibleApplication>(*args)
+}
---
+++ src/test/kotlin/bo/edu/ucb/bible/BibleApplicationTests.kt
@@ -0,0 +1,13 @@
+package bo.edu.ucb.bible
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BibleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
