304d5743cd5302af32e50948f669e035961503eb
carlaval655 <carla.val655@gmail.com>
2023-09-25 07:19:28 -0400

    Solucionando CORS

--- src/main/kotlin/bo/edu/ucb/bible/BibleApplication.kt
+++ src/main/kotlin/bo/edu/ucb/bible/BibleApplication.kt
@@ -2,7 +2,11 @@ package bo.edu.ucb.bible
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
+@CrossOrigin(origins = ["http://localhost:4200"])
 @SpringBootApplication
 class BibleApplication
 
